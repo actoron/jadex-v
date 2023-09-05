28e33689e0f6e26924efdfa19de3b735847a964a
Alexander Pokahr <ap@actoron.com>
2023-09-05 16:34:04 +0200

    add support for lambda agents and add lambda cleaner example

---
+++ application/micro/src/main/java/jadex/quickstart/cleanerworld/SimpleCleanerAgentLambda.java
@@ -0,0 +1,32 @@
+package jadex.quickstart.cleanerworld;
+
+import jadex.mj.feature.execution.SMjLambdaComponent;
+import jadex.quickstart.cleanerworld.environment.SensorActuator;
+import jadex.quickstart.cleanerworld.gui.SensorGui;
+
+/**
+ *  Agent without POJO class, just lambda body.
+ */
+public class SimpleCleanerAgentLambda
+{
+	/**
+	 *  Start the agent
+	 */
+	public static void main(String[] args)
+	{
+		SMjLambdaComponent.create(() ->
+		{
+			// Create the sensor/actuator interface object.
+			SensorActuator	actsense	= new SensorActuator();
+			
+			// Open a window showing the agent's perceptions
+			new SensorGui(actsense).setVisible(true);
+
+			// Agent uses one main loop for its random move behavior
+			while(true)
+			{
+				actsense.moveTo(Math.random(), Math.random());
+			}
+		});
+	}
+}
