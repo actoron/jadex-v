017fff3dd96368105b708d6e7ee5c59ec23b18e1
Alexander Pokahr <ap@actoron.com>
2023-10-24 15:15:49 +0200

    blocking lambda test

--- engine/base/mj-execution/src/main/java/jadex/mj/feature/execution/LambdaAgent.java
+++ engine/base/mj-execution/src/main/java/jadex/mj/feature/execution/LambdaAgent.java
@@ -5,6 +5,7 @@ import java.util.concurrent.Callable;
 import jadex.future.IFuture;
 import jadex.mj.core.ComponentIdentifier;
 import jadex.mj.core.IComponent;
+import jadex.mj.core.IThrowingConsumer;
 import jadex.mj.core.IThrowingFunction;
 import jadex.mj.core.impl.MjComponent;
 
@@ -68,5 +69,15 @@ public class LambdaAgent
 	{
 		MjComponent	comp = MjComponent.createComponent(MjComponent.class, () -> new MjComponent(null, cid));
 		return IMjExecutionFeature.getExternal(comp).scheduleStep(body);
+	}
+
+	/**
+	 *  Create a component and receive a result, when the body finishes.
+	 *  @param body	The code to be executed in the new component.
+	 */
+	public static <T> void	create(IThrowingConsumer<IComponent> body, ComponentIdentifier cid)
+	{
+		MjComponent	comp = MjComponent.createComponent(MjComponent.class, () -> new MjComponent(null, cid));
+		IMjExecutionFeature.getExternal(comp).scheduleStep(body);
 	}
 }
