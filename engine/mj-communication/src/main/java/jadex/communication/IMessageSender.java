ef7ea79f7854d0a60085a76b5ad0f4e218153efc
Kai <kj@actoron.com>
2023-10-30 17:47:21 +0100

    Refactored communication package names.

---
+++ engine/mj-communication/src/main/java/jadex/communication/IMessageSender.java
@@ -0,0 +1,20 @@
+package jadex.communication;
+
+import jadex.mj.core.ComponentIdentifier;
+
+/**
+ *  Interface for sending messages to components outside the current JVM.
+ *  
+ *  The implementation can be optimized for the local operating system, but
+ *  only one implementation should be used per-system to avoid incompatibility.
+ */
+public interface IMessageSender
+{
+	/**
+	 *  Sends a message to a component outside the current JVM.
+	 *  
+	 *  @param receiver The intended message receiver.
+	 *  @param message The message.
+	 */
+	public void sendMessage(ComponentIdentifier receiver, byte[] message);
+}
---
+++ engine/mj-communication/src/main/java/jadex/communication/UnixSocketStreamHandler.java
@@ -0,0 +1,54 @@
+package jadex.communication;
+
+import java.nio.channels.SocketChannel;
+import java.util.HashMap;
+
+import jadex.collection.RwMapWrapper;
+import jadex.mj.core.ComponentIdentifier;
+
+public class UnixSocketStreamHandler implements IMessageSender
+{
+	private RwMapWrapper<Long, SocketChannel> connectioncache;
+	
+	//private RwMapWrapper<ComponentIdentifier, IComponent>
+	
+	private volatile static UnixSocketStreamHandler singleton;
+	
+	/**
+	 *  Gets the singleton instance of the handler.
+	 *  @return Singleton instance of the handler.
+	 */
+	public static final UnixSocketStreamHandler get()
+	{
+		if (singleton == null)
+		{
+			synchronized (UnixSocketStreamHandler.class)
+			{
+				if (singleton == null)
+				{
+					singleton = new UnixSocketStreamHandler();
+				}
+			}
+		}
+		return singleton;
+	}
+	
+	/**
+	 *  Creates a new UnixSocketStreamHandler.
+	 */
+	private UnixSocketStreamHandler()
+	{
+		 connectioncache = new RwMapWrapper<>(new HashMap<>());
+	}
+	
+	/**
+	 *  Sends a message to a component outside the current JVM.
+	 *  
+	 *  @param receiver The intended message receiver.
+	 *  @param message The message.
+	 */
+	public void sendMessage(ComponentIdentifier receiver, byte[] message)
+	{
+		
+	}
+}
