03b2bba3418dba24a7db9065dd7ed0329f3516bc
lars <lars@lars-ThinkPad-T590>
2024-02-20 18:07:23 +0100

    script for starting hello world example

--- runHelloWorld.sh
+++ runHelloWorld.sh
@@ -23,5 +23,33 @@ java -cp \
 ./engine/requiredservicemicro/build/libs/requiredservicemicro.jar:\
 ./engine/serialization/build/libs/serialization.jar:\
 ./serialization/traverser/build/libs/traverser.jar:\
-~/.gradle/caches/modules-2/files-2.1/com.eclipsesource.minimal-json/minimal-json/0.9.5/abcdefghijk/minimal-json-0.9.5.jar \
+./serialization/json/build/libs/external-dependencies/minimal-json-0.9.5.jar:\
+./engine/bdi/build/libs/external-dependencies/asm-9.1.jar:\
+./engine/bdi/build/libs/external-dependencies/asm-tree-9.1.jar:\
+./engine/bdi/build/libs/external-dependencies/asm-util-9.1.jar:\
+./util/nativetools/build/libs/external-dependencies/jna-5.8.0.jar:\
+./engine/publishservicejetty/build/libs/external-dependencies/jakarta.annotation-api-2.0.0.jar:\
+./engine/publishservicejetty/build/libs/external-dependencies/jakarta.servlet-api-6.0.0.jar:\
+./engine/publishservicejetty/build/libs/external-dependencies/jakarta.transaction-api-2.0.0.jar:\
+./engine/publishservicejetty/build/libs/external-dependencies/jakarta.ws.rs-api-3.1.0.jar:\
+./engine/publishservicejetty/build/libs/external-dependencies/jetty-annotations-11.0.6.jar:\
+./engine/publishservicejetty/build/libs/external-dependencies/jetty-http-11.0.6.jar:\
+./engine/publishservicejetty/build/libs/external-dependencies/jetty-io-11.0.6.jar:\
+./engine/publishservicejetty/build/libs/external-dependencies/jetty-jakarta-servlet-api-5.0.2.jar:\
+./engine/publishservicejetty/build/libs/external-dependencies/jetty-jndi-11.0.6.jar:\
+./engine/publishservicejetty/build/libs/external-dependencies/jetty-plus-11.0.6.jar:\
+./engine/publishservicejetty/build/libs/external-dependencies/jetty-security-11.0.6.jar:\
+./engine/publishservicejetty/build/libs/external-dependencies/jetty-server-11.0.6.jar:\
+./engine/publishservicejetty/build/libs/external-dependencies/jetty-servlet-11.0.6.jar:\
+./engine/publishservicejetty/build/libs/external-dependencies/jetty-util-11.0.6.jar:\
+./engine/publishservicejetty/build/libs/external-dependencies/jetty-webapp-11.0.6.jar:\
+./engine/publishservicejetty/build/libs/external-dependencies/jetty-xml-11.0.6.jar:\
+./engine/publishservicejetty/build/libs/external-dependencies/slf4j-api-2.0.0-alpha1.jar:\
+./engine/publishservicejetty/build/libs/external-dependencies/websocket-core-common-11.0.6.jar:\
+./engine/publishservicejetty/build/libs/external-dependencies/websocket-core-server-11.0.6.jar:\
+./engine/publishservicejetty/build/libs/external-dependencies/jakarta.annotation-api-2.0.0.jar:\
+./engine/publishservicejetty/build/libs/external-dependencies/websocket-jetty-api-11.0.6.jar:\
+./engine/publishservicejetty/build/libs/external-dependencies/websocket-jetty-common-11.0.6.jar:\
+./engine/publishservicejetty/build/libs/external-dependencies/websocket-jetty-server-11.0.6.jar:\
+./engine/publishservicejetty/build/libs/external-dependencies/websocket-servlet-11.0.6.jar \
 jadex.micro.helloworld.HelloWorldAgent
