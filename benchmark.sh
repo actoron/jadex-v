c0ba3863a183daedf2f8d5f48fc910d783ae0594
Alexander Pokahr <ap@actoron.com>
2024-02-23 13:15:50 +0100

    add benchmark scripts

---
+++ benchmark.sh
@@ -0,0 +1,6 @@
+#/bin/sh
+./gradlew :benchmark-core:benchmark
+./gradlew :benchmark-execution:benchmark
+./gradlew :benchmark-micro:benchmark
+./gradlew :benchmark-bdi:benchmark
+./gradlew :benchmark-bpmn:benchmark
