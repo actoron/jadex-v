8ef736911df313a7edab378dac106f6ed4979396
Alexander Pokahr <ap@actoron.com>
2024-02-23 13:43:35 +0100

    cleanBenchmark before benchmark to force execution

--- benchmark.bat
+++ benchmark.bat
@@ -1,5 +1,5 @@
-call gradlew :benchmark-core:benchmark
-call gradlew :benchmark-execution:benchmark
-call gradlew :benchmark-micro:benchmark
-call gradlew :benchmark-bdi:benchmark
-call gradlew :benchmark-bpmn:benchmark
+call gradlew :benchmark-core:cleanBenchmark :benchmark-core:benchmark
+call gradlew :benchmark-execution:cleanBenchmark :benchmark-execution:benchmark
+call gradlew :benchmark-micro:cleanBenchmark :benchmark-micro:benchmark
+call gradlew :benchmark-bdi:cleanBenchmark :benchmark-bdi:benchmark
+call gradlew :benchmark-bpmn:cleanBenchmark :benchmark-bpmn:benchmark
--- benchmark.sh
+++ benchmark.sh
@@ -1,6 +1,6 @@
 #/bin/sh
-./gradlew :benchmark-core:benchmark
-./gradlew :benchmark-execution:benchmark
-./gradlew :benchmark-micro:benchmark
-./gradlew :benchmark-bdi:benchmark
-./gradlew :benchmark-bpmn:benchmark
+./gradlew :benchmark-core:cleanBenchmark :benchmark-core:benchmark
+./gradlew :benchmark-execution:cleanBenchmark :benchmark-execution:benchmark
+./gradlew :benchmark-micro:cleanBenchmark :benchmark-micro:benchmark
+./gradlew :benchmark-bdi:cleanBenchmark :benchmark-bdi:benchmark
+./gradlew :benchmark-bpmn:cleanBenchmark :benchmark-bpmn:benchmark
