55b597fa8ddc512471be8975356acf4126254175
Theresa Schulz <schuther@th-brandenburg.de>
2024-03-14 13:33:04 +0100

    Development in progress llmbdi

---
+++ bdi/llmbdi/src/main/java/jadex/bdi/llm/impl/LlmFeature.java
@@ -0,0 +1,8 @@
+package jadex.bdi.llm.impl;
+
+public class LlmFeature implements ILlmFeature
+{
+    public void generateAndExecutePlanStep(Goal goal, Plan plan, Object... context)
+    {
+    }
+}
