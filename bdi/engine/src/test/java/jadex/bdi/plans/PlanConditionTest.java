cdded639c59a19a461ce0594394af3fa9ad41239
Alexander Pokahr <ap@actoron.com>
2024-05-22 13:39:56 +0200

    add plan tests

---
+++ bdi/engine/src/test/java/jadex/bdi/plans/PlanConditionTest.java
@@ -0,0 +1,129 @@
+package jadex.bdi.plans;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import jadex.bdi.annotation.Belief;
+import jadex.bdi.annotation.Plan;
+import jadex.bdi.annotation.PlanAborted;
+import jadex.bdi.annotation.PlanBody;
+import jadex.bdi.annotation.PlanContextCondition;
+import jadex.bdi.annotation.PlanPrecondition;
+import jadex.bdi.annotation.Trigger;
+import jadex.bdi.runtime.IBDIAgent;
+import jadex.bdi.runtime.IPlan;
+import jadex.bdi.runtime.Val;
+import jadex.core.IExternalAccess;
+import jadex.future.Future;
+import jadex.micro.annotation.Agent;
+
+/**
+ *  Test plan pre- and context conditions
+ */
+public class PlanConditionTest
+{
+	@Agent(type="bdip")
+	static class PlanConditionTestAgent
+	{
+		@Belief
+		Val<Boolean>	bel	= new Val<>(true);
+
+		@Belief
+		List<String>	trigger	= new ArrayList<>();
+
+		Future<String>	prefut	= new Future<>();
+		Future<String>	contextfut	= new Future<>();
+		
+		@Plan(trigger=@Trigger(factadded="trigger"))
+		class PrePlan1
+		{
+			@PlanPrecondition
+			boolean precond()
+			{
+				return bel.get();
+			}
+			
+			@PlanBody
+			void body(IPlan plan)
+			{
+				prefut.setResultIfUndone(plan.getModelElement().getName());
+			}
+		}
+		
+		@Plan(trigger=@Trigger(factadded="trigger"))
+		class PrePlan2
+		{
+			@PlanPrecondition
+			boolean precond()
+			{
+				return !bel.get();
+			}
+			
+			@PlanBody
+			void body(IPlan plan)
+			{
+				prefut.setResultIfUndone(plan.getModelElement().getName());
+			}
+		}
+		
+		@Plan(trigger=@Trigger(factadded="trigger"))
+		class ContextPlan
+		{
+			@PlanContextCondition(beliefs="bel")
+			boolean context()
+			{
+				return bel.get();
+			}
+			
+			@PlanBody
+			void body(IPlan plan)
+			{
+				bel.set(false);
+			}
+			
+			@PlanAborted
+			void aborted()
+			{
+				contextfut.setResultIfUndone("aborted");				
+			}
+		}
+	}
+	
+	@Test
+	void testPlanPrecondition()
+	{
+		// First plan
+		{
+			PlanConditionTestAgent	pojo	= new PlanConditionTestAgent();
+			IExternalAccess	agent	= IBDIAgent.create(pojo);
+			agent.scheduleStep(() -> pojo.trigger.add("go"));
+			assertEquals(PlanConditionTestAgent.PrePlan1.class.getName(), pojo.prefut.get(1000));
+		}
+		
+		// Second plan
+		{
+			PlanConditionTestAgent	pojo	= new PlanConditionTestAgent();
+			IExternalAccess	agent	= IBDIAgent.create(pojo);
+			agent.scheduleStep(() ->
+			{
+				pojo.bel.set(false);
+				pojo.trigger.add("go");			
+			});
+			assertEquals(PlanConditionTestAgent.PrePlan2.class.getName(), pojo.prefut.get(1000));
+		}
+	}
+	
+	@Test
+	void testPlanContextcondition()
+	{
+		PlanConditionTestAgent	pojo	= new PlanConditionTestAgent();
+		IExternalAccess	agent	= IBDIAgent.create(pojo);
+		agent.scheduleStep(() -> pojo.trigger.add("go"));
+		assertEquals("aborted", pojo.contextfut.get(1000));
+	}
+}
+
---
+++ bdi/engine/src/test/java/jadex/bdi/plans/PlanPassedFailedAbortedTest.java
@@ -0,0 +1,108 @@
+package jadex.bdi.plans;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import jadex.bdi.annotation.Belief;
+import jadex.bdi.annotation.Plan;
+import jadex.bdi.annotation.PlanAborted;
+import jadex.bdi.annotation.PlanBody;
+import jadex.bdi.annotation.PlanContextCondition;
+import jadex.bdi.annotation.PlanFailed;
+import jadex.bdi.annotation.PlanPassed;
+import jadex.bdi.annotation.Trigger;
+import jadex.bdi.runtime.IBDIAgent;
+import jadex.bdi.runtime.IPlan;
+import jadex.bdi.runtime.PlanFailureException;
+import jadex.bdi.runtime.Val;
+import jadex.core.IExternalAccess;
+import jadex.future.Future;
+import jadex.micro.annotation.Agent;
+
+/**
+ *  Test plan pre- and context conditions
+ */
+public class PlanPassedFailedAbortedTest
+{
+	@Agent(type="bdip")
+	static class PlanPassedFailedAbortedTestAgent
+	{
+		@Belief
+		Val<String>	bel	= new Val<>(null);
+		
+		Future<String>	fut	= new Future<>();
+		
+		@Plan(trigger=@Trigger(factchanged="bel"))
+		class TestPlan
+		{
+			@PlanContextCondition(beliefs="bel")
+			boolean context()
+			{
+				return bel.get()!=null;
+			}
+			
+			@PlanBody
+			void body(IPlan plan)
+			{
+				if("pass".equals(bel.get()))
+				{
+					// nop
+				}
+				else if("fail".equals(bel.get()))
+				{
+					throw new PlanFailureException("fail");
+				}
+				else if("abort".equals(bel.get()))
+				{
+					bel.set(null);
+				}				
+			}
+			
+			@PlanPassed
+			void passed()
+			{
+				fut.setResultIfUndone("passed");				
+			}
+			
+			@PlanFailed
+			void failed()
+			{
+				fut.setResultIfUndone("failed");				
+			}
+			
+			@PlanAborted
+			void aborted()
+			{
+				fut.setResultIfUndone("aborted");				
+			}
+		}
+	}
+	
+	@Test
+	void testPlanPassed()
+	{
+		PlanPassedFailedAbortedTestAgent	pojo	= new PlanPassedFailedAbortedTestAgent();
+		IExternalAccess	agent	= IBDIAgent.create(pojo);
+		agent.scheduleStep(() -> pojo.bel.set("pass"));
+		assertEquals("passed", pojo.fut.get(1000));
+	}
+	
+	@Test
+	void testPlanFailed()
+	{
+		PlanPassedFailedAbortedTestAgent	pojo	= new PlanPassedFailedAbortedTestAgent();
+		IExternalAccess	agent	= IBDIAgent.create(pojo);
+		agent.scheduleStep(() -> pojo.bel.set("fail"));
+		assertEquals("failed", pojo.fut.get(1000));
+	}
+	
+	@Test
+	void testPlanAborted()
+	{
+		PlanPassedFailedAbortedTestAgent	pojo	= new PlanPassedFailedAbortedTestAgent();
+		IExternalAccess	agent	= IBDIAgent.create(pojo);
+		agent.scheduleStep(() -> pojo.bel.set("abort"));
+		assertEquals("aborted", pojo.fut.get(1000));
+	}
+}
