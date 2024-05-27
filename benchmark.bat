8e1c09fee59ac5e4954b32031c3f12f0b96b56cb
Alexander Pokahr <ap@actoron.com>
2024-05-27 10:42:05 +0200

    run benchmarks in same gradle instance

--- benchmark.bat
+++ benchmark.bat
@@ -1,5 +1,1 @@
-call gradlew :benchmark-core:cleanBenchmark :benchmark-core:benchmark
-call gradlew :benchmark-execution:cleanBenchmark :benchmark-execution:benchmark
-call gradlew :benchmark-micro:cleanBenchmark :benchmark-micro:benchmark
-call gradlew :benchmark-bdi:cleanBenchmark :benchmark-bdi:benchmark
-call gradlew :benchmark-bpmn:cleanBenchmark :benchmark-bpmn:benchmark
+call gradlew cleanBenchmark benchmark --max-workers 1
--- benchmark.sh
+++ benchmark.sh
@@ -1,6 +1,2 @@
 #/bin/sh
-./gradlew :benchmark-core:cleanBenchmark :benchmark-core:benchmark
-./gradlew :benchmark-execution:cleanBenchmark :benchmark-execution:benchmark
-./gradlew :benchmark-micro:cleanBenchmark :benchmark-micro:benchmark
-./gradlew :benchmark-bdi:cleanBenchmark :benchmark-bdi:benchmark
-./gradlew :benchmark-bpmn:cleanBenchmark :benchmark-bpmn:benchmark
+./gradlew cleanBenchmark benchmark --max-workers 1
