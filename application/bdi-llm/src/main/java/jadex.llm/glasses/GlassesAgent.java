79fc657d0ee3ec3dcdaabb65d135159112a04bd0
Kai <kj@actoron.com>
2024-07-08 15:11:01 +0200

    Moved LLM to feature branch.

---
+++ application/bdi-llm/src/main/java/jadex.llm/glasses/GlassesAgent.java
@@ -0,0 +1,93 @@
+package jadex.llm.glasses;
+
+import jadex.bdi.annotation.*;
+import jadex.bdi.llm.ILlmFeature;
+import jadex.bdi.model.BDIModelLoader;
+import jadex.bdi.runtime.IBDIAgentFeature;
+import jadex.bdi.runtime.IPlan;
+import jadex.core.IComponent;
+import jadex.micro.annotation.Agent;
+import jadex.model.annotation.OnStart;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.objectweb.asm.ClassReader;
+
+@Agent(type = "bdi")
+public class GlassesAgent
+{
+    static
+    {
+        System.out.println("GlassesAgent class loaded");
+    }
+
+    @Belief
+    private final SortBy characteristic = SortBy.SHAPE;
+
+    public enum SortBy
+    {
+        SHAPE, MATERIAL, COLOUR, BRAND, MODEL
+    }
+
+    @Belief
+    private List<Glasses> glassesList = new ArrayList<>();
+    private GlassesSortingGoal sortingGoal;
+    private ILlmFeature llmFeature;
+
+    public class GlassesSortingGoal
+    {
+        private SortBy sortBy;
+
+        public GlassesSortingGoal(SortBy sortBy) {
+            this.sortBy = sortBy;
+        }
+
+        public SortBy getSortBy() {
+            return sortBy;
+        }
+    }
+
+    @Goal
+    class SortGlassesListGoal
+    {
+        @GoalResult
+        public List<Glasses> getSortedGlassesList() {
+            return glassesList;
+        }
+    }
+
+    @Plan(trigger = @Trigger(goals = SortGlassesListGoal.class))
+    public void executeGeneratedPlan(IPlan context)
+    {
+        System.out.println("executeGeneratedPlan is called");
+
+        SortGlassesListGoal sortingGoal = new SortGlassesListGoal();
+        List<Glasses> glassesList = getGlassesList();
+
+        llmFeature.generateAndExecutePlanStep(this, sortingGoal, glassesList);
+    }
+
+    private List<Glasses> getGlassesList()
+    {
+        return null;
+    }
+
+
+    @OnStart
+    public void body(IComponent agent)
+    {
+        System.out.println("GlassesAgent active");
+
+        // call method from Glasses
+        glassesList = Glasses.generateRandomGlassesList();
+        sortingGoal = new GlassesSortingGoal(characteristic);
+        // call method from class reader
+        BDIModelLoader loader = new BDIModelLoader();
+        ClassReader classReader = new ClassReader(loader);
+        classReader.readAllClassStructure();
+
+        agent.getFeature(IBDIAgentFeature.class).dispatchTopLevelGoal(new SortGlassesListGoal()).get();
+        System.out.println("GlassesAgent finished");
+    }
+}
---
+++ application/bdi-llm/src/main/java/jadex.llm/glasses/Main.java
@@ -0,0 +1,23 @@
+package jadex.llm.glasses;
+
+import jadex.bdi.runtime.BDICreationInfo;
+import jadex.bdi.runtime.IBDIAgent;
+import jadex.core.IComponent;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+        String apiKey = args[0];
+        String classpath = args[1];
+
+        IBDIAgent.create(new BDICreationInfo("jadex.llm.glasses.GlassesAgent")
+                .addArgument("apiKey", apiKey)
+                .addArgument("classpath", classpath));
+
+        IComponent.waitForLastComponentTerminated();
+
+
+    }
+}
