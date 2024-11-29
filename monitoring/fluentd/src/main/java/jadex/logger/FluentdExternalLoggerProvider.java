3e59453bcb566c471bb9d5a42e13a449fc8bbb16
lars <lb@actoron.com>
2024-11-29 14:41:38 +0100

    fix

--- monitoring/fluentd/src/main/java/jadex/logger/FluentdExternalLoggerProvider.java
+++ monitoring/fluentd/src/main/java/jadex/logger/FluentdExternalLoggerProvider.java
@@ -4,8 +4,8 @@ import java.lang.System.Logger;
 
 public class FluentdExternalLoggerProvider implements IExternalLoggerProvider
 {
-	public Logger getLogger(String name)
+	public Logger getLogger(String name, boolean system)
 	{
-		return new FluentdLogger(name, true);
+		return new FluentdLogger(name, system);
 	}
 }
--- monitoring/graylog/src/main/java/jadex/logger/GraylogExternalLoggerProvider.java
+++ monitoring/graylog/src/main/java/jadex/logger/GraylogExternalLoggerProvider.java
@@ -6,9 +6,9 @@ import org.graylog2.logging.GelfHandler;
 
 public class GraylogExternalLoggerProvider implements IExternalLoggerProvider
 {
-	public Logger getLogger(String name)
+	public Logger getLogger(String name, boolean system)
 	{
-		GraylogLogger ret = new GraylogLogger(name);
+		GraylogLogger ret = new GraylogLogger(name, system);
 		java.util.logging.Logger logger = ret.getLoggerImplementation();
         logger.setUseParentHandlers(false);
         GelfHandler handler = new GelfHandler();
