b851ad84e7ee65802f2040027cd47bc4f4110a4c
Theresa Schulz <schuther@th-brandenburg.de>
2025-01-23 15:26:57 +0100

    Working on dataset

--- application/bdi-llm/src/main/java/jadex.llm/glasses/Glasses.java
+++ application/bdi-llm/src/main/java/jadex.llm/glasses/Glasses.java
@@ -1,6 +1,6 @@
 package jadex.llm.glasses;
 
-import org.json.JSONArray;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 import java.io.FileWriter;
@@ -8,46 +8,6 @@ import java.io.IOException;
 import java.util.*;
 
 class Glasses
-{
-    private int articleNumber;
-    private String brand;
-    private int frameWidth;
-    private int frameHeight;
-    private String colour;
-
-    public Glasses(int articleNumber, String brand, int frameWidth, int frameHeight, String colour)
-    {
-        this.articleNumber = articleNumber;
-        this.brand = brand;
-        this.frameWidth = frameWidth;
-        this.frameHeight = frameHeight;
-        this.colour = colour;
-    }
-
-    public JSONObject toJSON()
-    {
-        JSONObject json = new JSONObject();
-        json.put("articleNumber", articleNumber);
-        json.put("brand", brand);
-        json.put("frameWidth", frameWidth);
-        json.put("frameHeight", frameHeight);
-        json.put("colour", colour);
-        return json;
-    }
-
-    public String toString()
-    {
-        return "Glasses{" +
-                "articleNumber=" + articleNumber +
-                ", brand='" + brand + '\'' +
-                ", frameWidth=" + frameWidth +
-                ", frameHeight=" + frameHeight +
-                ", colour='" + colour + '\'' +
-                '}';
-    }
-}
-
-class Customer
 {
     private int customerId;
     private String firstName;
@@ -57,9 +17,13 @@ class Customer
     private int postalCode;
     private String city;
     private String dayOfBirth;
-    private Glasses glasses;
+    private int articleNumber;
+    private String brand;
+    private int frameWidth;
+    private int frameHeight;
+    private String colour;
 
-    public Customer(int customerId, String firstName, String lastName, String street, int houseNumber, int postalCode, String city, String dayOfBirth, Glasses glasses)
+    public Glasses(int customerId, String firstName, String lastName, String street, int houseNumber, int postalCode, String city, String dayOfBirth, int articleNumber, String brand, int frameWidth, int frameHeight, String colour)
     {
         this.customerId = customerId;
         this.firstName = firstName;
@@ -69,11 +33,14 @@ class Customer
         this.postalCode = postalCode;
         this.city = city;
         this.dayOfBirth = dayOfBirth;
-        this.glasses = glasses;
+        this.articleNumber = articleNumber;
+        this.brand = brand;
+        this.frameWidth = frameWidth;
+        this.frameHeight = frameHeight;
+        this.colour = colour;
     }
 
-
-    public JSONObject toJSON()
+    public JSONObject toJSONObject()
     {
         JSONObject json = new JSONObject();
         json.put("customerId", customerId);
@@ -84,80 +51,87 @@ class Customer
         json.put("postalCode", postalCode);
         json.put("city", city);
         json.put("dayOfBirth", dayOfBirth);
-        json.put("glasses", glasses.toJSON());
+        json.put("articleNumber", articleNumber);
+        json.put("brand", brand);
+        json.put("frameWidth", frameWidth);
+        json.put("frameHeight", frameHeight);
+        json.put("colour", colour);
         return json;
     }
 
-    public String toString()
+    public static List<Glasses> generateOpticanDB()
     {
-        return "Customer{" +
-                "customerId=" + customerId +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", street='" + street + '\'' +
-                ", houseNumber=" + houseNumber +
-                ", postalCode=" + postalCode +
-                ", city='" + city + '\'' +
-                ", dayOfBirth='" + dayOfBirth + '\'' +
-                ", glasses=" + glasses +
-                '}';
-    }
-
-    static List<Glasses> generateRandomGlassesList()
-    {
-        ArrayList<Glasses> glassesList = new ArrayList<>();
-        Set<Integer> articleNumber = new HashSet<>();
+        ArrayList<Glasses> customerList = new ArrayList<>();
+        Set<Integer> articleNumbers = new HashSet<>();
+        Set<Integer> customerIds = new HashSet<>();
 
         Random random = new Random();
 
+        String[] firstNames = {"Alice", "Emma", "Olaf"};
+        String[] lastNames = {"Meier", "Schmidt", "Braun"};
+        String[] streets = {"Streetlane", "Lanestreet", "Avenue"};
+        String[] cities = {"City1", "City2", "City3"};
         String[] brands = {"Brand1", "Brand2", "Brand3"};
         String[] colours = {"red", "black", "yellow"};
 
-        for (int i = 0; i < 20; i++)
+
+        for (int i = 0; i < 10; i++)
         {
-            int articleNumberGen;
+            // Generate unique 4-digit customer ID
+            int customerId;
             do
             {
-                articleNumberGen = 100000 + random.nextInt(900000); // Generate unique 6-digit article number
-            } while (!articleNumber.add(articleNumberGen));
+                customerId = 1000 + random.nextInt(9000);
+            } while (!customerIds.add(customerId));
+
+            // Generate unique 6-digit article number
+            int articleNumber;
+            do
+            {
+                articleNumber = 100000 + random.nextInt(900000);
+            } while (!articleNumbers.add(articleNumber));
 
             Glasses glasses = new Glasses(
-                    articleNumberGen,
+                    customerId,
+                    firstNames[random.nextInt(firstNames.length)],
+                    lastNames[random.nextInt(lastNames.length)],
+                    streets[random.nextInt(streets.length)],
+                    random.nextInt(99) + 1, // Random house number 1-99
+                    10000 + random.nextInt(90000), // Random postal code
+                    cities[random.nextInt(cities.length)],
+                    "199" + (random.nextInt(10) + 1) + "-0" + (random.nextInt(9) + 1) + "-" + (random.nextInt(28) + 1), // Random DOB in 1990s
+                    articleNumber,
                     brands[random.nextInt(brands.length)],
                     130 + random.nextInt(50), // Frame width between 130-180
                     40 + random.nextInt(30),  // Frame height between 40-70
                     colours[random.nextInt(colours.length)]
             );
 
-            glassesList.add(glasses);
+            customerList.add(glasses);
         }
-        return glassesList;
+        return customerList;
     }
 
-    public static void generateDatasetJSON(List<Glasses> glassesList)
-    {
-        JSONArray glassesArray = new JSONArray();
-        for (Glasses glasses : glassesList)
-        {
-            glassesArray.put(glasses.toJSON());
-        }
-        try (FileWriter file = new FileWriter("Dataset.json"))
-        {
-            try
-            {
-                file.write(glassesArray.toString());
-            } catch (IOException e)
-            {
-                throw new RuntimeException(e);
-            }
-            file.flush();
-            System.out.println("Dataset.json created");
-        } catch (IOException e)
-        {
-            System.out.println("An error occurred.");
-        }
-    }
+//    public static void saveDataToJson()
+//    {
+//        String path = "application/bdi-llm/src/main/java/jadex.llm/glasses/Dataset1.json";
+//        JSONArray jsonArray = new JSONArray();
+//
+//        for (Glasses glasses : customerList) {
+//            jsonArray.add(glasses.toJSONObject());
+//        }
+//
+//        try (FileWriter file = new FileWriter(path)) {
+//            file.write(jsonArray.toString());
+//            System.out.println("Dataset.json file created successfully!");
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
 }
+
+
+
 
 
 
