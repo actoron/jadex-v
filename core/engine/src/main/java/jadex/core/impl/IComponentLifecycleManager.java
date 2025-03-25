46a2fc8bb1ce620c6dc57c7b5a76a9f5b29bb4f0
Alexander Pokahr <ap@actoron.com>
2025-03-25 15:33:25 +0100

    support result subscription in lifecycle manager

--- core/engine/src/main/java/jadex/core/impl/IComponentLifecycleManager.java
+++ core/engine/src/main/java/jadex/core/impl/IComponentLifecycleManager.java
@@ -2,10 +2,12 @@ package jadex.core.impl;
 
 import java.util.Map;
 
+import jadex.common.NameValue;
 import jadex.core.Application;
 import jadex.core.ComponentIdentifier;
 import jadex.core.IComponent;
 import jadex.core.IComponentHandle;
+import jadex.future.ISubscriptionIntermediateFuture;
 
 /**
  *  Managing POJOs by creating/running/terminating corresponding components.
@@ -41,4 +43,13 @@ public interface IComponentLifecycleManager
 	 *  Fetch the result(s) of the POJO.
 	 */
 	public Map<String, Object> getResults(Object pojo);
+	
+	/**
+	 *  Listen to results of the pojo.
+	 *  @throws UnsupportedOperationException when subscription is not supported
+	 */
+	public default ISubscriptionIntermediateFuture<NameValue> subscribeToResults(Object pojo)
+	{
+		throw new UnsupportedOperationException();
+	}
 }
