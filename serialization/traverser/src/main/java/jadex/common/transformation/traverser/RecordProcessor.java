9a8e267825f0ee7e7f247bc9816c79a72bcd9d36
Alexander Pokahr <ap@actoron.com>
2025-03-28 15:03:55 +0100

    deep copy for records

--- serialization/traverser/src/main/java/jadex/common/transformation/traverser/RecordProcessor.java
+++ serialization/traverser/src/main/java/jadex/common/transformation/traverser/RecordProcessor.java
@@ -1,9 +1,14 @@
 package jadex.common.transformation.traverser;
 
-import java.awt.Rectangle;
+import java.lang.invoke.MethodHandles;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 
+import jadex.common.SReflect;
+import jadex.common.SUtil;
 import jadex.common.transformation.IStringConverter;
 import jadex.common.transformation.traverser.Traverser.MODE;
 
@@ -33,7 +38,32 @@ public class RecordProcessor implements ITraverseProcessor
 	 */
 	public Object process(Object object, Type type, Traverser traverser, List<ITraverseProcessor> conversionprocessors, List<ITraverseProcessor> processors, IStringConverter converter, MODE mode, ClassLoader targetcl, Object context)
 	{
+		// Record doesn't adhere to bean spec, grrr.
+		Class<?> clazz = SReflect.getClass(type);
+		Field[]	fields	= clazz.getDeclaredFields();
+		List<Object>	args	= new ArrayList<Object>(fields.length);
+		List<Class<?>>	types	= new ArrayList<Class<?>>(fields.length);
+		try
+		{
+			for(Field f: fields)
+			{
+				f.setAccessible(true);
+				Object val	= MethodHandles.lookup().unreflectGetter(f).invoke(object);
+				args.add(traverser.doTraverse(val, f.getType(), conversionprocessors, processors, converter, mode, targetcl, context));
+				types.add(f.getType());
+			}
+			
+			if(targetcl!=null)
+				clazz	= SReflect.findClass(clazz.getName(), null, targetcl);
+			
+			Constructor<?>	c	= clazz.getConstructor(types.toArray(new Class[types.size()]));
+			object	= MethodHandles.lookup().unreflectConstructor(c).invokeWithArguments(args);
+		}
+		catch(Throwable t)
+		{
+			SUtil.throwUnchecked(t);
+		}
+
 		return object;
 	}
 }
-
