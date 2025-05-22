39178b95883a2ba9f3b7adf22ae168254215354e
Alexander Pokahr <ap@actoron.com>
2025-05-22 15:14:47 +0200

    revert to normal build

--- benchmark.sh
+++ benchmark.sh
@@ -1,3 +1,3 @@
 #/bin/sh
-#./gradlew cleanBenchmark benchmark -x benchmark-thirdparty:benchmark --max-workers 1
-./gradlew cleanBenchmark benchmark-bt:benchmark --max-workers 1
+./gradlew cleanBenchmark benchmark -x benchmark-thirdparty:benchmark --max-workers 1
+#./gradlew cleanBenchmark benchmark-bt:benchmark --max-workers 1
