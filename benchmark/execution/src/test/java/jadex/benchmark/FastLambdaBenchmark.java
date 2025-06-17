96cd3971c9d855e93134123faf8498ec733432c3
Alexander Pokahr <ap@actoron.com>
2025-06-17 20:55:33 +0200

    more realistic fast lambda usage

--- benchmark/execution/src/test/java/jadex/benchmark/FastLambdaBenchmark.java
+++ benchmark/execution/src/test/java/jadex/benchmark/FastLambdaBenchmark.java
@@ -6,6 +6,7 @@ import jadex.core.ComponentIdentifier;
 import jadex.core.IComponent;
 import jadex.core.IThrowingFunction;
 import jadex.core.impl.Component;
+import jadex.core.impl.ComponentManager;
 import jadex.execution.impl.FastLambda;
 import jadex.future.Future;
 
@@ -21,18 +22,16 @@ public class FastLambdaBenchmark
 //		// Otherwise, SReflect.hasGui() will hang, because swing tries to get a logger on init.
 //		// Happens, because we create a component before the manager and use a future which triggers startNotifications() wich uses isGuiThread!?
 //		System.getLogger("jadex.benchmark.FastLambdaBenchmark");
+		// Now "fixed" in SUtil.isGuiThread() by checking just the thread name instead of using SReflect.hasGui().
 		
 		BenchmarkHelper.benchmarkMemory(() -> 
 		{
-			IThrowingFunction<IComponent, ComponentIdentifier>	body	= comp ->{return comp.getId();};
+			IThrowingFunction<IComponent, ComponentIdentifier>	body	= comp -> comp.getId();
 			// No handle is returned when creating fast lambdas, so we need to use a Future to get the handle.
-			Future<IComponent>	comp	= new Future<>();
-			Component.createComponent(FastLambda.class, () -> new FastLambda<>(body, null, false)
-			{{
-				comp.setResult(this);
-			}});
-			IComponent	thecomp	= comp.get();
-			return () -> thecomp.getComponentHandle().terminate().get();
+			Future<ComponentIdentifier>	res	= new Future<>();
+			Component.createComponent(FastLambda.class, () -> new FastLambda<>(body, res, false));
+			ComponentIdentifier	thecomp	= res.get();
+			return () -> ComponentManager.get().getComponent(thecomp).getComponentHandle().terminate().get();
 		});
 	}
 }
