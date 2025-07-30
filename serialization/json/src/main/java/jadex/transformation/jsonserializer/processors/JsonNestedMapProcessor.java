58e590ec5b0c64a767c1c63bd0f34ac8c15b3be9
Theresa Schulz <schuther@th-brandenburg.de>
2025-07-30 15:23:40 +0200

    Fixed certain edge cases in JsonNestedMapProcessor.

--- serialization/json/src/main/java/jadex/transformation/jsonserializer/processors/JsonNestedMapProcessor.java
+++ serialization/json/src/main/java/jadex/transformation/jsonserializer/processors/JsonNestedMapProcessor.java
@@ -12,14 +12,28 @@ import jadex.common.SReflect;
  */
 public class JsonNestedMapProcessor extends JsonMapProcessor
 {
+
 	/**
 	 *  Test if the processor is applicable.
 	 *  @param object The object.
 	 *  @param targetcl	If not null, the traverser should make sure that the result object is compatible with the class loader,
 	 *    e.g. by cloning the object using the class loaded from the target class loader.
-	 *  @return True, if is applicable. 
+	 *  @return True, if is applicable.
 	 */
-	public boolean isApplicable(Object object, Type type, boolean clone, ClassLoader targetcl)
+	protected boolean isApplicable(Object object, Type type, ClassLoader targetcl, JsonReadContext context)
+	{
+		Class<?> clazz = SReflect.getClass(type);
+		return object instanceof JsonObject && (clazz==null || SReflect.isSupertype(Map.class, clazz));
+	}
+
+	/**
+	 *  Test if the processor is applicable.
+	 *  @param object The object.
+	 *  @param targetcl	If not null, the traverser should make sure that the result object is compatible with the class loader,
+	 *    e.g. by cloning the object using the class loaded from the target class loader.
+	 *  @return True, if is applicable.
+	 */
+	protected boolean isApplicable(Object object, Type type, ClassLoader targetcl, JsonWriteContext context)
 	{
 		Class<?> clazz = SReflect.getClass(type);
 		return object instanceof JsonObject && (clazz==null || SReflect.isSupertype(Map.class, clazz));
@@ -35,7 +49,13 @@ public class JsonNestedMapProcessor extends JsonMapProcessor
 	{
 		Class<?> valclazz = val!=null? val.getClass(): null;
 		if(JsonObject.class.equals(valclazz))
+		{
+			valclazz = Map.class;
+		} else
+		{
 			valclazz = null;
+		}
+
 		return valclazz;
 	}
 }
