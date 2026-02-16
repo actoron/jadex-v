fa34840c465f0a059443224d1bfe3d9bd8306d10
Alexander Pokahr <ap@actoron.com>
2026-02-16 13:11:30 +0100

    run benchmark as component

--- benchmark/execution/src/test/java/jadex/benchmark/LambdaAgentBenchmark.java
+++ benchmark/execution/src/test/java/jadex/benchmark/LambdaAgentBenchmark.java
@@ -12,10 +12,15 @@ public class LambdaAgentBenchmark
 	@Test
 	void benchmarkTime()
 	{
-		BenchmarkHelper.benchmarkTime(() -> 
-		{
-			IComponentManager.get().run(comp -> comp.getId()).get();
-		});
+		// Run inside component for better comparison to previous version without result scheduling
+		// -> otherwise would schedule on global runner and then wake up to main thread
+		// -> now it only wakes up caller component.
+		IComponentManager.get().run((Runnable) () ->
+			BenchmarkHelper.benchmarkTime(() -> 
+			{
+				IComponentManager.get().run(comp -> comp.getId()).get();
+			})
+		).get();
 	}
 
 	public static void main(String[] args)
