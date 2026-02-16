faebb924b8157501b0d4fe130d227f3003553981
Alexander Pokahr <ap@actoron.com>
2026-02-16 13:42:39 +0100

    Revert "run benchmark as component"
    This reverts commit fa34840c465f0a059443224d1bfe3d9bd8306d10.

--- benchmark/execution/src/test/java/jadex/benchmark/LambdaAgentBenchmark.java
+++ benchmark/execution/src/test/java/jadex/benchmark/LambdaAgentBenchmark.java
@@ -12,15 +12,10 @@ public class LambdaAgentBenchmark
 	@Test
 	void benchmarkTime()
 	{
-		// Run inside component for better comparison to previous version without result scheduling
-		// -> otherwise would schedule on global runner and then wake up to main thread
-		// -> now it only wakes up caller component.
-		IComponentManager.get().run((Runnable) () ->
-			BenchmarkHelper.benchmarkTime(() -> 
-			{
-				IComponentManager.get().run(comp -> comp.getId()).get();
-			})
-		).get();
+		BenchmarkHelper.benchmarkTime(() -> 
+		{
+			IComponentManager.get().run(comp -> comp.getId()).get();
+		});
 	}
 
 	public static void main(String[] args)
