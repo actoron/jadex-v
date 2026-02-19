ae13750247b68977fc03920f80b9871a39964caf
Alexander Pokahr <ap@actoron.com>
2026-02-19 13:08:56 +0100

    nop bt cleaner benchmark

--- benchmark/bt/src/test/java/jadex/benchmark/BTCleanerBenchmark.java
+++ benchmark/bt/src/test/java/jadex/benchmark/BTCleanerBenchmark.java
@@ -14,24 +14,24 @@ public class BTCleanerBenchmark	extends BTAbstractCleanerBenchmark
 	@Test
 	void benchmarkTime()
 	{
-		BenchmarkHelper.benchmarkTime(() -> 
-		{
-			Future<Void> ret = new Future<>();
-			IComponentHandle agent = IComponentManager.get().create(new BTCleanerBenchmarkAgent(ret, envid)).get();
-			ret.get();
-			agent.terminate().get();
-		});
+//		BenchmarkHelper.benchmarkTime(() -> 
+//		{
+//			Future<Void> ret = new Future<>();
+//			IComponentHandle agent = IComponentManager.get().create(new BTCleanerBenchmarkAgent(ret, envid)).get();
+//			ret.get();
+//			agent.terminate().get();
+//		});
 	}
 
 	@Test
 	void benchmarkMemory()
 	{
-		BenchmarkHelper.benchmarkMemory(() -> 
-		{
-			Future<Void> ret = new Future<>();
-			IComponentHandle agent = IComponentManager.get().create(new BTCleanerBenchmarkAgent(ret, envid)).get();
-			ret.get();
-			return () -> agent.terminate().get();
-		});
+//		BenchmarkHelper.benchmarkMemory(() -> 
+//		{
+//			Future<Void> ret = new Future<>();
+//			IComponentHandle agent = IComponentManager.get().create(new BTCleanerBenchmarkAgent(ret, envid)).get();
+//			ret.get();
+//			return () -> agent.terminate().get();
+//		});
 	}
 }
