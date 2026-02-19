30649a48625b1e4e8499ced15cba4eaccb9ef646
Alexander Pokahr <ap@actoron.com>
2026-02-19 20:29:40 +0100

    Revert "skip cleaner"
    This reverts commit 8d04ff28ec232cfe3e77e48f045c501a0a998221.

--- benchmark/bt/src/test/java/jadex/benchmark/BTCleanerBenchmark.java
+++ benchmark/bt/src/test/java/jadex/benchmark/BTCleanerBenchmark.java
@@ -16,19 +16,19 @@ import jadex.logger.ILoggingFeature;
  */
 public class BTCleanerBenchmark
 {
-//	static String envid;
-//	static
-//	{
-//		IComponentManager.get().getFeature(ILoggingFeature.class).setSystemLoggingLevel(Level.ERROR);
-//		IComponentManager.get().getFeature(ILoggingFeature.class).setAppLoggingLevel(Level.WARNING);
-//
-//		int fps = 0; // steps / frames per second: 0 -> disable steps
-//		CleanerworldEnvironment env = IComponentManager.get().create(new CleanerworldEnvironment(fps)).get().getPojoHandle(CleanerworldEnvironment.class);
-//		env.createWorld().get();
-//		envid = Environment.add(env);
-//	}
-//	
-//	@Test
+	static String envid;
+	static
+	{
+		IComponentManager.get().getFeature(ILoggingFeature.class).setSystemLoggingLevel(Level.ERROR);
+		IComponentManager.get().getFeature(ILoggingFeature.class).setAppLoggingLevel(Level.WARNING);
+
+		int fps = 0; // steps / frames per second: 0 -> disable steps
+		CleanerworldEnvironment env = IComponentManager.get().create(new CleanerworldEnvironment(fps)).get().getPojoHandle(CleanerworldEnvironment.class);
+		env.createWorld().get();
+		envid = Environment.add(env);
+	}
+	
+	@Test
 	void benchmarkTime()
 	{
 //		BenchmarkHelper.benchmarkTime(() -> 
@@ -52,14 +52,14 @@ public class BTCleanerBenchmark
 //		});
 //	}
 	
-//	public static void	main(String[] args)
-//	{
-//		for(;;) 
-//		{
-//			Future<Void> ret = new Future<>();
-//			IComponentHandle agent = IComponentManager.get().create(new BTCleanerBenchmarkAgent(ret, envid)).get();
-//			ret.get();
-//			agent.terminate().get();
-//		}
-//	}
+	public static void	main(String[] args)
+	{
+		for(;;) 
+		{
+			Future<Void> ret = new Future<>();
+			IComponentHandle agent = IComponentManager.get().create(new BTCleanerBenchmarkAgent(ret, envid)).get();
+			ret.get();
+			agent.terminate().get();
+		}
+	}
 }
