ee4e7f5cf716482421840cd4e5156f1f30167377
Alexander Pokahr <ap@actoron.com>
2026-02-19 21:14:31 +0100

    skip cleaner

--- benchmark/bt/src/test/java/jadex/benchmark/BTCleanerBenchmark.java
+++ benchmark/bt/src/test/java/jadex/benchmark/BTCleanerBenchmark.java
@@ -16,50 +16,50 @@ import jadex.logger.ILoggingFeature;
  */
 public class BTCleanerBenchmark
 {
-	static String envid;
-	static
-	{
-		IComponentManager.get().getFeature(ILoggingFeature.class).setSystemLoggingLevel(Level.ERROR);
-		IComponentManager.get().getFeature(ILoggingFeature.class).setAppLoggingLevel(Level.WARNING);
-
-		int fps = 0; // steps / frames per second: 0 -> disable steps
-		CleanerworldEnvironment env = IComponentManager.get().create(new CleanerworldEnvironment(fps)).get().getPojoHandle(CleanerworldEnvironment.class);
-		env.createWorld().get();
-		envid = Environment.add(env);
-	}
-	
-	@Test
-	void benchmarkTime()
-	{
-//		BenchmarkHelper.benchmarkTime(() -> 
+//	static String envid;
+//	static
+//	{
+//		IComponentManager.get().getFeature(ILoggingFeature.class).setSystemLoggingLevel(Level.ERROR);
+//		IComponentManager.get().getFeature(ILoggingFeature.class).setAppLoggingLevel(Level.WARNING);
+//
+//		int fps = 0; // steps / frames per second: 0 -> disable steps
+//		CleanerworldEnvironment env = IComponentManager.get().create(new CleanerworldEnvironment(fps)).get().getPojoHandle(CleanerworldEnvironment.class);
+//		env.createWorld().get();
+//		envid = Environment.add(env);
+//	}
+//	
+//	@Test
+//	void benchmarkTime()
+//	{
+////		BenchmarkHelper.benchmarkTime(() -> 
+////		{
+////			Future<Void> ret = new Future<>();
+////			IComponentHandle agent = IComponentManager.get().create(new BTCleanerBenchmarkAgent(ret, envid)).get();
+////			ret.get();
+////			agent.terminate().get();
+////		});
+//	}
+//
+////	@Test
+////	void benchmarkMemory()
+////	{
+////		BenchmarkHelper.benchmarkMemory(() -> 
+////		{
+////			Future<Void> ret = new Future<>();
+////			IComponentHandle agent = IComponentManager.get().create(new BTCleanerBenchmarkAgent(ret, envid)).get();
+////			ret.get();
+////			return () -> agent.terminate().get();
+////		});
+////	}
+//	
+//	public static void	main(String[] args)
+//	{
+//		for(;;) 
 //		{
 //			Future<Void> ret = new Future<>();
 //			IComponentHandle agent = IComponentManager.get().create(new BTCleanerBenchmarkAgent(ret, envid)).get();
 //			ret.get();
 //			agent.terminate().get();
-//		});
-	}
-
-//	@Test
-//	void benchmarkMemory()
-//	{
-//		BenchmarkHelper.benchmarkMemory(() -> 
-//		{
-//			Future<Void> ret = new Future<>();
-//			IComponentHandle agent = IComponentManager.get().create(new BTCleanerBenchmarkAgent(ret, envid)).get();
-//			ret.get();
-//			return () -> agent.terminate().get();
-//		});
+//		}
 //	}
-	
-	public static void	main(String[] args)
-	{
-		for(;;) 
-		{
-			Future<Void> ret = new Future<>();
-			IComponentHandle agent = IComponentManager.get().create(new BTCleanerBenchmarkAgent(ret, envid)).get();
-			ret.get();
-			agent.terminate().get();
-		}
-	}
 }
