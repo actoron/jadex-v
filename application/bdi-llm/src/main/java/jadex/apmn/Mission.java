294fdb19de23d397d9af74dd927b77a9c9987cfd
Theresa Schulz <schuther@th-brandenburg.de>
2026-02-25 16:02:15 +0100

    Work on JSONmapping for reading Mission.json

--- application/bdi-llm/src/main/java/jadex/apmn/Mission.java
+++ application/bdi-llm/src/main/java/jadex/apmn/Mission.java
@@ -1,54 +1,35 @@
 package jadex.apmn;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.Map;
-
 public class Mission
 {
     private String belief;
     private String goal;
-    private Mission mission;
-    private String file = "/home/admin-schuther/Documents/Coding/jadex-v/application/bdi-llm/src/main/java/jadex/apmn/Mission.json";
 
-    ObjectMapper mapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    public Mission(){}
 
-    @JsonProperty("mission")
-    public void unpackNested(Map<String, Object> mission)
+    public Mission(String belief, String goal)
     {
-        this.belief = (String)mission.get("belief");
-        this.goal = (String)mission.get("goal");
+        this.belief = belief;
+        this.goal = goal;
     }
 
-    public void getBelief()
+    public String getBelief()
     {
-        try
-        {
-            mission = mapper.readValue(new File(file), Mission.class);
-        }
-        catch (IOException e)
-        {
-            throw new RuntimeException(e);
-        }
-        belief = mission.belief;
-        System.out.println(belief);
+        return belief;
     }
 
-    public void getGoal()
+    public void setBelief(String belief)
     {
-        try
-        {
-            mission = mapper.readValue(new File(file), Mission.class);
-        }
-        catch (IOException e)
-        {
-            throw new RuntimeException(e);
-        }
-        goal = mission.goal;
-        System.out.println(goal);
+        this.belief = belief;
+    }
+
+    public String getGoal()
+    {
+        return goal;
+    }
+
+    public void setGoal(String goal)
+    {
+        this.goal = goal;
     }
 }
--- application/bdi-llm/src/main/java/jadex/apmn/Starter.java
+++ application/bdi-llm/src/main/java/jadex/apmn/Starter.java
@@ -1,16 +1,50 @@
 package jadex.apmn;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jadex.core.IComponentManager;
+import jakarta.json.Json;
+import jakarta.json.JsonReader;
+import jakarta.json.JsonStructure;
+
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Starter
 {
+    private static String FILE = "application/bdi-llm/src/main/java/jadex/apmn/Mission.json";
+
     public static void main(String[] args)
     {
+        try
+        {
+            JsonReader reader = Json.createReader(new FileReader(FILE));
+            JsonStructure json = reader.read();
+            System.out.println(json);
+        }
+        catch (IOException e)
+        {
+            throw new RuntimeException(e);
+        }
+
+
+        ObjectMapper mapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        try
+        {
+            Mission mission = mapper.readValue(new File(FILE), Mission.class);
+            System.out.println(mission.getBelief());
+            System.out.println(mission.getGoal());
+        }
+        catch (IOException e)
+        {
+            throw new RuntimeException(e);
+        }
+
         IComponentManager.get().create(new AgentActor());
         System.out.println("Hallo");
-        Mission aom = new Mission();
-        aom.getBelief();
-        aom.getGoal();
+
         IComponentManager.get().waitForLastComponentTerminated();
     }
 }
