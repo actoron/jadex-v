dab2b9c75e7bae0b8ee9e1071dddc3c39130e5c1
Theresa Schulz <schuther@th-brandenburg.de>
2026-03-02 16:43:39 +0100

    Work on JSONmapping for reading Mission.json

--- application/bdi-llm/src/main/java/jadex/apmn/Mission.java
+++ application/bdi-llm/src/main/java/jadex/apmn/Mission.java
@@ -2,15 +2,13 @@ package jadex.apmn;
 
 public class Mission
 {
+    private int id;
     private String belief;
     private String goal;
 
-    public Mission(){}
-
-    public Mission(String belief, String goal)
+    public int getId()
     {
-        this.belief = belief;
-        this.goal = goal;
+        return id;
     }
 
     public String getBelief()
@@ -18,14 +16,19 @@ public class Mission
         return belief;
     }
 
-    public void setBelief(String belief)
-    {
-        this.belief = belief;
-    }
-
     public String getGoal()
     {
         return goal;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public void setBelief(String belief)
+    {
+        this.belief = belief;
     }
 
     public void setGoal(String goal)
---
+++ application/bdi-llm/src/main/java/jadex/apmn/MissionList.java
@@ -0,0 +1,18 @@
+package jadex.apmn;
+
+import java.util.List;
+
+public class MissionList
+{
+    private List<Mission> missions;
+
+    public List<Mission> getMissions()
+    {
+        return missions;
+    }
+
+    public void setMissions(List<Mission> missions)
+    {
+        this.missions = missions;
+    }
+}
