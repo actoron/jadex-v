46380cd831cbe62b6ff4c1bd8de20bd80efb793f
Alexander Pokahr <ap@actoron.com>
2026-03-27 12:40:57 +0100

    fix model not reloaded

--- application/bdi-service/src/main/java/jadex/bdi/blocksworld/LlmBlocksworldAgent.java
+++ application/bdi-service/src/main/java/jadex/bdi/blocksworld/LlmBlocksworldAgent.java
@@ -270,54 +270,8 @@ public class LlmBlocksworldAgent	extends BlocksworldAgent	implements IBlocksworl
 			};
 			provider.addActionListener(fetchmodels);
 			
-			IComponentHandle llmagent = IComponentManager.get().create(
-				new LlmResultAgent(LlmHelper.createChatModel(
-					(Provider)provider.getSelectedItem(),
-					(String)model.getSelectedItem(),
-					think.isSelected())
-				)).get();
-			LlmResultAgent llmagentpojo = llmagent.getPojoHandle(LlmResultAgent.class);
-			int[] last = new int[] {0};
-			llmagent.subscribeToResults().next(event ->
-			{
-				if(event.type()==Type.ADDED && event.name().equals("response"))
-				{
-					if(last[0]!=1)
-					{
-						append(center, "\n", null);
-						last[0]=1;
-					}
-					append(center, ""+event.value(), null);
-				}
-				else if(event.type()==Type.ADDED && event.name().equals("thinking"))
-				{
-					if(last[0]!=2)
-					{
-						append(center, "\n", thinking);
-						last[0]=2;
-					}
-					append(center, ""+event.value(), thinking);
-				}
-				else if(event.type()==Type.ADDED && event.name().equals("toolcalls"))
-				{
-					if(last[0]!=3)
-					{
-						append(center, "\n", toolcall);
-						last[0]=3;
-					}
-					append(center, event.value()+"\n", toolcall);
-				}
-				else if(event.type()==Type.ADDED && event.name().equals("toolresults"))
-				{
-					if(last[0]!=3)
-					{
-						append(center, "\n", toolcall);
-						last[0]=3;
-					}
-					append(center, event.value()+"\n", toolcall);
-				}
-			}).printOnEx();
-			
+			LlmResultAgent[] llmagentpojo = new LlmResultAgent[1];
+
 			ActionListener	al	= e ->
 			{
 				if(e.getSource()==prompt && !e.getActionCommand().equals("comboBoxEdited"))
@@ -334,7 +288,7 @@ public class LlmBlocksworldAgent	extends BlocksworldAgent	implements IBlocksworl
 				prompt.setEnabled(false);
 				send.setEnabled(false);
 				
-				IFuture<Void>	done	= llmagentpojo.chat((String)prompt.getSelectedItem(),
+				IFuture<Void>	done	= llmagentpojo[0].chat((String)prompt.getSelectedItem(),
 					sendimage.isSelected() ? new Image[]{png} : new Image[0]);
 				
 				done.then(v -> {prompt.setEnabled(true); send.setEnabled(true); append(center, "\n==============\n", null);})
@@ -349,6 +303,61 @@ public class LlmBlocksworldAgent	extends BlocksworldAgent	implements IBlocksworl
 			worlds.add(panel, 0);
 			worlds.validate();
 			worlds.repaint();
+			
+			model.addItemListener(e ->
+			{
+				if(e.getStateChange()==java.awt.event.ItemEvent.SELECTED)
+				{
+					center.setText("");
+					IComponentHandle llmagent = IComponentManager.get().create(
+							new LlmResultAgent(LlmHelper.createChatModel(
+								(Provider)provider.getSelectedItem(),
+								(String)model.getSelectedItem(),
+								think.isSelected())
+							)).get();
+					llmagentpojo[0] = llmagent.getPojoHandle(LlmResultAgent.class);
+					int[] last = new int[] {0};
+					llmagent.subscribeToResults().next(event ->
+					{
+						if(event.type()==Type.ADDED && event.name().equals("response"))
+						{
+							if(last[0]!=1)
+							{
+								append(center, "\n", null);
+								last[0]=1;
+							}
+							append(center, ""+event.value(), null);
+						}
+						else if(event.type()==Type.ADDED && event.name().equals("thinking"))
+						{
+							if(last[0]!=2)
+							{
+								append(center, "\n", thinking);
+								last[0]=2;
+							}
+							append(center, ""+event.value(), thinking);
+						}
+						else if(event.type()==Type.ADDED && event.name().equals("toolcalls"))
+						{
+							if(last[0]!=3)
+							{
+								append(center, "\n", toolcall);
+								last[0]=3;
+							}
+							append(center, event.value()+"\n", toolcall);
+						}
+						else if(event.type()==Type.ADDED && event.name().equals("toolresults"))
+						{
+							if(last[0]!=3)
+							{
+								append(center, "\n", toolcall);
+								last[0]=3;
+							}
+							append(center, event.value()+"\n", toolcall);
+						}
+					}).printOnEx();
+				}
+			});
 			
 			SwingUtilities.invokeLater(() ->fetchmodels.actionPerformed(null));
 		});
