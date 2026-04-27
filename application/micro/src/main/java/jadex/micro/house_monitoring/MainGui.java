ef3e9d8c9decf673cbdb2c22f33025e56dcbc92f
Alexander Pokahr <ap@actoron.com>
2026-04-27 12:11:27 +0200

    fix size

--- application/micro/src/main/java/jadex/micro/house_monitoring/MainGui.java
+++ application/micro/src/main/java/jadex/micro/house_monitoring/MainGui.java
@@ -79,7 +79,7 @@ public class MainGui
 //					terminateComponents();
 				}
 			});
-			frame.pack();
+			frame.setSize(800, 600);
 			frame.setLocationRelativeTo(null);
 			frame.setVisible(true);
 		});
